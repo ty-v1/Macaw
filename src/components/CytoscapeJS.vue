f02c0cbbcd6a95cc3a3a03abd08ed6f9f0ee6fa5
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2018-10-27 20:12:31 +0900

    Delete sample data

--- src/components/CytoscapeJS.vue
+++ src/components/CytoscapeJS.vue
@@ -37,28 +37,7 @@
 
         protected setUpGraphLibrary(): void {
             const cytoscapeOptions: CytoscapeOptions = {
-                container: document.getElementById('cytoscape'),
-                elements: {
-                    nodes: [
-                        {data: {id: 'cat'}},
-                        {data: {id: 'bird'}},
-                        {data: {id: 'ladybug'}},
-                        {data: {id: 'aphid'}},
-                        {data: {id: 'rose'}},
-                        {data: {id: 'grasshopper'}},
-                        {data: {id: 'plant'}},
-                        {data: {id: 'wheat'}}
-                    ],
-                    edges: [
-                        {data: {source: 'cat', target: 'bird'}},
-                        {data: {source: 'bird', target: 'ladybug'}},
-                        {data: {source: 'bird', target: 'grasshopper'}},
-                        {data: {source: 'grasshopper', target: 'plant'}},
-                        {data: {source: 'grasshopper', target: 'wheat'}},
-                        {data: {source: 'ladybug', target: 'aphid'}},
-                        {data: {source: 'aphid', target: 'rose'}}
-                    ]
-                },
+                container: document.getElementById('cytoscape')
             };
             this.cytoscapeCore = CytoscapeInitializer.initialize(cytoscapeOptions);
         }
