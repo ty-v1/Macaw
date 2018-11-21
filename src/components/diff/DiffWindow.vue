86d8262500dd99d56b23d15d4439f90d4fb8029d
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2018-11-21 23:24:26 +0900

    Add Diff

---
+++ src/components/diff/DiffWindow.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="content">
+        <DiffList v-for="variantId in variantIds"
+                  :key="variantId"
+                  :variant-id="variantId">
+        </DiffList>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import DiffList from "./DiffList.vue";
+    import {mapGetters} from "vuex";
+
+    @Component(
+        {
+            components: {DiffList},
+            computed: {
+                ...mapGetters(
+                    {
+                        variantIds: 'DiffStore/variantIds'
+                    }),
+            },
+        })
+    export default class DiffWindow extends Vue {
+    }
+</script>
+
+<style scoped>
+    .content {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow: scroll;
+    }
+</style>
