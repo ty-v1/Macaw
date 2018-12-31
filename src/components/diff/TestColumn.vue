89f1d1874c18fc76ad9646933e5f791f42b52d3c
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2018-12-31 21:10:36 +0900

    Add TestDetail component

---
+++ src/components/diff/TestColumn.vue
@@ -0,0 +1,27 @@
+<template>
+    <tr>
+        <td class="fqn">{{testResult.fqn}}</td>
+        <td class="result">{{result}}</td>
+    </tr>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from "vue-property-decorator";
+    import {TestResult} from "../../scripts/data/TestResult";
+
+    @Component
+    export default class TestColumn extends Vue {
+        @Prop() testResult!: TestResult;
+
+        get result(): string {
+            return this.testResult.isSuccess ? "pass" : "fail";
+        }
+    }
+</script>
+
+<style scoped>
+    td {
+        text-align: left;
+        word-wrap: break-word;
+    }
+</style>
